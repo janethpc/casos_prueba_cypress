3fbae4fabfc35c661cb1d904cfddce4b0c5cb0dd
Bertha Janeth Pérez Canales <janeth_2124@hotmail.com>
2023-07-15 23:28:15 -0600

    prueba orange

---
+++ cypress/e2e/smoktestorange.cy.js
@@ -0,0 +1,12 @@
+describe("test suite - caso de pruebas orange", () => {
+    
+
+    it('login', () => {
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+        cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
+        cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
+        cy.get('.orangehrm-login-branding > img').should("be.visible")
+        
+    })
+
+})
